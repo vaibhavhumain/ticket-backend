54bd207fdf7365841f11987543b67a6d166364d3
Akash Dhiman <akashdhiman@192.168.1.10>
2025-09-08 11:40:48 +0530

    fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const User = require("../models/User");
 
 exports.register = async (req, res) => {
   try {
