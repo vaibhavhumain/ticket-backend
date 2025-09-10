4333c28c55f0e855cea433d701aac050601ce19f
vaibhav <codweird5@gmail.com>
2025-09-10 12:20:41 +0530

    Aded,pf'

--- models/User.js
+++ models/User.js
@@ -4,14 +4,25 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }, 
+    password: { type: String, required: true },
+
     role: {
       type: String,
-      enum: ["employee", "developer", "admin"],
-      default: "employee",
+      enum: [
+        "HR",
+        "IT",
+        "Sales",
+        "Marketing",
+        "Production",
+        "Purchase",
+        "Accounts",
+        "Admin",
+      ],
+      default: "IT", // 👈 set whichever you want as default
     },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("User", userSchema);
+module.exports =
+  mongoose.models.User || mongoose.model("User", userSchema);
